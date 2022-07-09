63e75e030ae7ccd03ae3b16025d6f1e4da0028c9
nocom <69950817+shaikhabd@users.noreply.github.com>
2022-07-09 19:55:12 +0530

    3 new tools added

--- src/routes/tools.svelte
+++ src/routes/tools.svelte
@@ -5,9 +5,18 @@ import Navbar from "../components/Navbar.svelte";
 <Navbar />
 <section style="padding: 3%;">
 <h1 style="font-weight: 600; font-size: xx-large; color: #4caae7;">Available Tools</h1><br>
-<div style="display:flex; justify-content:space-evenly">
+<!-- <div style="display:flex; justify-content:space-evenly">
 <button style="display:flex;align-items:center; justify-content:center; font-weight: 600;color:black;width: 50vw;height: 10vh; font-size:larger ;background-color:#22b36c;border: none;"><a href="https://share.streamlit.io/shaikhabd/tools/main/tool2.py">Fragments generation of smiles</a></button>
+</div> -->
+<div style="display:flex; justify-content:space-evenly">
+<button style="display:flex;align-items:center; justify-content:center; font-weight: 600;color:black;width: 50vw;height: 10vh; font-size:larger ;background-color:#22b36c;border: none;"><a href="https://molsoft.com/mprop/mprop.cgi">Drug-Likeness and molecular property prediction</a></button>
 </div>
+<div style="display:flex; justify-content:space-evenly">
+  <button style="display:flex;align-items:center; justify-content:center; font-weight: 600;color:black;width: 50vw;height: 10vh; font-size:larger ;background-color:#22b36c;border: none;"><a href="https://molview.org/">Molecular View</a></button>
+  </div>
+  <div style="display:flex; justify-content:space-evenly">
+    <button style="display:flex;align-items:center; justify-content:center; font-weight: 600;color:black;width: 50vw;height: 10vh; font-size:larger ;background-color:#22b36c;border: none;"><a href="http://chemmine.ucr.edu/eisearch/query/">Structural Similarity Search</a></button>
+    </div>
 </section>
 
 <style>
@@ -36,6 +45,10 @@ a:hover {
 button:hover{
     background-color: white;
 }
+  div{
+     position: relative;
+     margin: 1%;
+  }
 </style>
 
 
