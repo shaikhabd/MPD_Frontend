1a000f71c95d9728ff208bfe33d54d9f45c9e275
nocom <69950817+shaikhabd@users.noreply.github.com>
2022-07-17 12:46:48 +0530

    added new tool

--- src/routes/tools.svelte
+++ src/routes/tools.svelte
@@ -9,6 +9,9 @@ import Navbar from "../components/Navbar.svelte";
 <button style="display:flex;align-items:center; justify-content:center; font-weight: 600;color:black;width: 50vw;height: 10vh; font-size:larger ;background-color:#22b36c;border: none;"><a href="https://share.streamlit.io/shaikhabd/tools/main/tool2.py">Fragments generation of smiles</a></button>
 </div> -->
 <div style="display:flex; justify-content:space-evenly">
+  <button style="display:flex;align-items:center; justify-content:center; font-weight: 600;color:black;width: 50vw;height: 10vh; font-size:larger ;background-color:#22b36c;border: none;"><a href="https://mitmpd-moledits.netlify.app">Drug-Likeness and molecular property prediction</a></button>
+  </div>
+<div style="display:flex; justify-content:space-evenly">
 <button style="display:flex;align-items:center; justify-content:center; font-weight: 600;color:black;width: 50vw;height: 10vh; font-size:larger ;background-color:#22b36c;border: none;"><a href="https://molsoft.com/mprop/mprop.cgi">Drug-Likeness and molecular property prediction</a></button>
 </div>
 <div style="display:flex; justify-content:space-evenly">
@@ -18,7 +21,7 @@ import Navbar from "../components/Navbar.svelte";
     <button style="display:flex;align-items:center; justify-content:center; font-weight: 600;color:black;width: 50vw;height: 10vh; font-size:larger ;background-color:#22b36c;border: none;"><a href="http://chemmine.ucr.edu/eisearch/query/">Structural Similarity Search</a></button>
     </div>
 </section>
-
+https://mitmpd-moledits.netlify.app/
 <style>
     a{
     font-size: large;
