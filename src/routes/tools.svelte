4b72c4fbf81921c07f26dc8c54a83d6ffaed1cd3
nocom <69950817+shaikhabd@users.noreply.github.com>
2022-07-17 12:52:24 +0530

    added new tool plus changes

--- src/routes/tools.svelte
+++ src/routes/tools.svelte
@@ -9,7 +9,7 @@ import Navbar from "../components/Navbar.svelte";
 <button style="display:flex;align-items:center; justify-content:center; font-weight: 600;color:black;width: 50vw;height: 10vh; font-size:larger ;background-color:#22b36c;border: none;"><a href="https://share.streamlit.io/shaikhabd/tools/main/tool2.py">Fragments generation of smiles</a></button>
 </div> -->
 <div style="display:flex; justify-content:space-evenly">
-  <button style="display:flex;align-items:center; justify-content:center; font-weight: 600;color:black;width: 50vw;height: 10vh; font-size:larger ;background-color:#22b36c;border: none;"><a href="https://mitmpd-moledits.netlify.app">Drug-Likeness and molecular property prediction</a></button>
+  <button style="display:flex;align-items:center; justify-content:center; font-weight: 600;color:black;width: 50vw;height: 10vh; font-size:larger ;background-color:#22b36c;border: none;"><a href="https://mitmpd-moledits.netlify.app">Molecular Editor</a></button>
   </div>
 <div style="display:flex; justify-content:space-evenly">
 <button style="display:flex;align-items:center; justify-content:center; font-weight: 600;color:black;width: 50vw;height: 10vh; font-size:larger ;background-color:#22b36c;border: none;"><a href="https://molsoft.com/mprop/mprop.cgi">Drug-Likeness and molecular property prediction</a></button>
